b8839bebba413026de9d4dc5060a7016bf5bb003
+ <+>
2023-06-13 20:43:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader=document.querySelector("h1")
+newHeader.setAttribute("id","victory")
+document.getElementById("victory").innerHTML="Wanjira is a champion"
+
+

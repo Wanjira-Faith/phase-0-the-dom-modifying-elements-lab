0aacd4934413a899ab6e359b5f48c6fd10bdb333
+ <+>
2023-06-20 12:12:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Write your code here!
-main.remove();
-const newHeader=document.querySelector("h1")
-newHeader.setAttribute("id","victory")
-document.getElementById("victory").innerHTML="Wanjira is a champion"
+//const body=document.body
+//const div=document.createElement('div)
+//body.append(div)
 
+//const div=document.querySelector("div")
+//.innerText-prints what is seen on the screen
+//.textContent-prints everything in the html
+//div.innerHTML="<strong>Hello World</strong>" -0nly way to add an HTML element from a string into an element like this
+
+// const strong=document.createElement("strong")
+// strong.textContent="Hello World"
+// div.append(strong)
+
+document.querySelector("main").remove();
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Wanjira is the champion"
 
